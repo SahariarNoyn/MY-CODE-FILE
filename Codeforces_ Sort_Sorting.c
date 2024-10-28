bc0697f8c16eb2d0e34db62e748e801edd98c62d
Md. Al Sahariar Noyn <noyon20202@gmail.com>
2024-10-29 00:41:12 +0600

    abc

--- Codeforces_ Sort_Sorting.c
+++ Codeforces_ Sort_Sorting.c
@@ -6,7 +6,7 @@ void bubbleSort(char str[], int n) {
     for (i = 0; i < n - 1; i++) {
         for (j = 0; j < n - i - 1; j++) {
             if (str[j] > str[j + 1]) {
-                // Swap str[j] and str[j + 1]
+                
                 temp = str[j];
                 str[j] = str[j + 1];
                 str[j + 1] = temp;
@@ -17,14 +17,13 @@ void bubbleSort(char str[], int n) {
 
 int main() {
     int N;
-    scanf("%d", &N); // Read the size of the string
-    char S[N + 1]; // Declare an array to hold the string (N + 1 for null terminator)
+    scanf("%d", &N); 
+    char S[N + 1]; 
 
-    scanf("%s", S); // Read the string
+    scanf("%s", S); 
 
-    bubbleSort(S, N); // Sort the string
-
-    printf("%s\n", S); // Print the sorted string
+    bubbleSort(S, N);
+    printf("%s\n", S);
 
     return 0;
 }
